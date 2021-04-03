d4bff695f462edb98fec98e30f06120b1ff8f927
DESKTOP-9OG4IRR\JeongEunHye <wjddmsgpp@naver.com>
2021-04-03 20:33:52 +0900

    수정

--- resources/js/front_ui.js
+++ resources/js/front_ui.js
@@ -24,6 +24,15 @@ $(function () {
 
 	var wh = $(window).height();
 	$('.main-banner-wrap').height(wh);
+
+	var container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
+	var options = { //지도를 생성할 때 필요한 기본 옵션
+		center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
+		level: 3 //지도의 레벨(확대, 축소 정도)
+	};
+
+	var map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+
 });
 
 $(window).on('resize', function () {
