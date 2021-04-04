735151b0f0abfba27beafdfa115142a6f07f8b32
DESKTOP-9OG4IRR\JeongEunHye <wjddmsgpp@naver.com>
2021-04-04 21:46:37 +0900

    no message

--- resources/js/front_ui.js
+++ resources/js/front_ui.js
@@ -55,6 +55,26 @@ function mapApi(){
 	};
 
 	var map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+
+	var markers = [
+		{
+			position: new kakao.maps.LatLng(37.387337, 127.122438)
+		},
+		{
+			position: new kakao.maps.LatLng(37.387337, 127.122438),
+			text: '라온스퀘어' // text 옵션을 설정하면 마커 위에 텍스트를 함께 표시할 수 있습니다
+		}
+	];
+
+	var staticMapContainer  = document.getElementById('staticMap'), // 이미지 지도를 표시할 div
+		staticMapOption = {
+			center: new kakao.maps.LatLng(37.387337, 127.122438), // 이미지 지도의 중심좌표
+			level: 3, // 이미지 지도의 확대 레벨
+			marker: markers // 이미지 지도에 표시할 마커
+		};
+
+// 이미지 지도를 생성합니다
+	var staticMap = new kakao.maps.StaticMap(staticMapContainer, staticMapOption);
 }
 
 /**********************************************************************************
